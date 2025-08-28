45a0fc83659c44fcab456eb1bdec30cf88e9be7d
Maurice Kappelhof <github@emailmaurice.com>
2025-08-28 07:37:10 +0200

    Fix background script

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,3 @@
 export const isFirefox = typeof browser !== 'undefined';
 export const storage = isFirefox ? browser.storage : chrome.storage;
+export const webRequest = isFirefox ? browser.webRequest : chrome.webRequest;
