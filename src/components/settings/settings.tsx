e625fb9805f80a8cf4c96ba2086787b04378e657
Maurice Kappelhof <github@emailmaurice.com>
2025-09-18 09:27:59 +0200

    Disable export button when there are no headers

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,12 +1,18 @@
+import { useCallback } from 'react';
 import { Button } from '@components/button/button';
 import { IMPORT_PARAM } from '@constants/index';
+import { useHeaderTweakerContext } from '@contexts/headertweaker.context';
 import { exportHeaders } from '@helpers/header.helper';
 import { ArrowDownTrayIcon, ArrowUpTrayIcon } from '@heroicons/react/24/solid';
 
 import css from './settings.module.scss';
 
 export const Settings = () => {
-  const openImportWindow = () => {
+  const { headers } = useHeaderTweakerContext();
+
+  const headerCount = headers.length;
+
+  const openImportWindow = useCallback(() => {
     const width = 800;
     const height = 500;
     const left = window.screen.availWidth - width - 100;
@@ -16,16 +22,16 @@ export const Settings = () => {
       'headertweaker',
       `width=${width},height=${height},left=${left},top=${top},menubar=no,toolbar=no,location=no,status=no,scrollbars=yes,resizable=yes`
     );
-  };
+  }, []);
 
   return (
     <div className={css.root}>
       <Button onClick={openImportWindow}>
         <ArrowUpTrayIcon /> Import new headers
       </Button>
-      <Button onClick={exportHeaders}>
+      <Button onClick={exportHeaders} disabled={headerCount < 1}>
         <ArrowDownTrayIcon />
-        Export existing headers
+        Export {headerCount >= 1 ? headerCount : ''} header{headerCount !== 1 ? 's' : ''}
       </Button>
     </div>
   );
