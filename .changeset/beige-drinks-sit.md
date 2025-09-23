ae6891544ddfe61cb5fc74e1d2e787b6441bfb75
Maurice Kappelhof <github@emailmaurice.com>
2025-09-23 19:48:14 +0200

    Cleanup & add deploy action (#8)
    * Cleanup old stuff
    
    * Add changeset
    
    * Add deploy action
    
    * remove
    
    * Add pre-commit hook

---
+++ .changeset/beige-drinks-sit.md
@@ -0,0 +1,5 @@
+---
+"headertweaker": patch
+---
+
+Cleanup
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,10 +1,1 @@
 export type Header = { id: string; name: string; value: string; enabled: boolean };
-
-export type RenderFn = () => Promise<void>;
-
-export type PanelElement = {
-  element: string;
-  content?: string;
-  attributes?: Record<string, string>;
-  listenTo?: Record<string, (event: Event) => void>;
-};
